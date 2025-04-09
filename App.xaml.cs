945a3e8431e171ce26427782269b8bcc07863232
AskWhyNot <tommarvoloriddle1975@gmail.com>
2025-04-09 21:02:11 +0700

    add astra plugin, fix app.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace IFLEGameLauncher
@@ -8,6 +9,25 @@ namespace IFLEGameLauncher
     {
         public static string AccessToken { get; internal set; }
         public static string RefreshToken { get; internal set; }
+        static App()
+        {
+            string nativePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Astra", Environment.Is64BitProcess ? "x64" : "x32");
+            Environment.SetEnvironmentVariable("PATH", nativePath + ";" + Environment.GetEnvironmentVariable("PATH"));
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Astra.Context.Initialize();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            Astra.Context.Terminate();
+        }
     }
 
 }
